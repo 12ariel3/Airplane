7360613dadf6a30361eee274b8cc6a680e2ed058
12ariel3 <losioariel@hotmail.com>
2023-04-22 18:43:55 -0300

    updated

--- Assets/_Scripts/LevelLoader.cs
+++ Assets/_Scripts/LevelLoader.cs
@@ -13,9 +13,6 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            LoadNextLevel();
-        }
     }
 
 
